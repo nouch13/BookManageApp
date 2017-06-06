e333b84e3b9f3e707a33e97396ccfbd9664af778
nouch13 <0tamar0pokemon@gmail.com>
2017-06-06 18:55:47 +0900

    ズームへの対応化

--- app/src/main/java/com/github/nouch13/bookmanageapp/MainActivity.kt
+++ app/src/main/java/com/github/nouch13/bookmanageapp/MainActivity.kt
@@ -3,11 +3,15 @@ package com.github.nouch13.bookmanageapp
 import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.DragEvent
+import android.view.KeyEvent
 import android.view.Menu
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
+
+    val home_address: String = "http://www.uec.ac.jp/"
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,13 +20,14 @@ class MainActivity : AppCompatActivity() {
 
         // レイアウトで指定のWebViewIdを指定
         val myWebView: WebView = findViewById(R.id.webView1) as WebView
-
         // 標準ブラウザ起動させない
-        myWebView.loadUrl("http://www.uec.ac.jp/")
-
+        myWebView.setWebViewClient(WebViewClient())
+        // 最初に開くページの指定
+        myWebView.loadUrl(home_address)
         // JavaScriptを許可する
         myWebView.settings?.javaScriptEnabled = true
-
+        // ズームを有効化
+        myWebView.settings?.builtInZoomControls = true
     }
 
 }
